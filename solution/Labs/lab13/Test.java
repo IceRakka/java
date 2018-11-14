1b0359922781e5d410296a2f7b314a672711a25a
Xiaoxue Ma <xm576@nyu.edu>
2018-11-13 23:49:29 -0500

    finish proj0

--- solution/Labs/lab13/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        String[] test = new String[]{"cat", "elephant", "ball", "fuck", "apple", "giant", "dick"};
-        String[] sorted = RadixSort.sort(test);
-        for (int i = 0; i < sorted.length; i++) {
-            System.out.println(sorted[i]);
-        }
-    }
-}
--- solution/Projects/Applications of DLList/OffByOne.java
+++
@@ -1,6 +0,0 @@
-public class OffByOne implements CharacterComparator {
-    @Override
-    public boolean equalChars(char x, char y) {
-        return Math.abs(y - x) == 1;
-    }
-}
