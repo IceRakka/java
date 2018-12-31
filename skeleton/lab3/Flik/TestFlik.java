f43005c4fc059cd55df864a36142e2b3cf105bb1
Xiaoxue Ma <xm576@nyu.edu>
2018-12-30 23:20:35 -0500

    lab3

---
+++ skeleton/lab3/Flik/TestFlik.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestFlik {
+    @Test
+    public void TestFlik(){
+        assertTrue(128==128);
+        assertTrue(Flik.isSameNumber(128, 128));
+    }
+}
