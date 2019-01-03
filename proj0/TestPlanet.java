cd56afecb40998e51db6dc45c101d2b20e5d2985
Xiaoxue Ma <xm576@nyu.edu>
2019-01-03 16:34:05 -0500

    test

---
+++ proj0/TestPlanet.java
@@ -0,0 +1,30 @@
+public class TestPlanet{
+
+	private static void checkEquals(double actual, double expected, String label, double eps){
+		if (Math.abs(expected-actual) <= eps * Math.max(expected, actual)){
+			System.out.println("PASS:" + label + ":Expected " + expected + " and you gave "+actual);
+		} else {
+			System.out.println("FAIL:" + label + ":Expected " + expected + " and you gave "+actual);
+		}
+	}
+
+	private static void checkPlanet(){
+		System.out.println("Checking Planet...");
+
+		Planet p1 = new Planet(1.0,1.0,3.0,4.0,5.0,"jupiter.gif");
+		Planet p2 = new Planet(2.0,1.0,3.0,4.0,4e11,"jupiter.gif");
+		Planet p3 = new Planet(4.0,5.0,3.0,4.0,5.0,"jupiter.gif");
+
+		checkEquals(p1.calcForceExertedBy(p2),133.4,"Planet",0.01);
+		checkEquals(p1.calcForceExertedBy(p3),6.67e-11,"Planet",0.01);
+
+		System.out.println("Pairwise force would be:");
+		System.out.println("p1&p2: " + p1.calcForceExertedBy(p2));
+		System.out.println("p1&p3: " + p1.calcForceExertedBy(p3));
+		System.out.println("p2&p3: " + p2.calcForceExertedBy(p3));
+	}
+
+	public static void main(String[] args){
+		checkPlanet();
+	}
+}
