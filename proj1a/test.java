8ce3d13f25ed42b78d020221f9a2bec12732ead8
Xiaoxue Ma <xm576@nyu.edu>
2019-01-04 20:43:27 -0500

    proj 1a

---
+++ proj1a/test.java
@@ -0,0 +1,25 @@
+public 
+
+         ArrayDeque.addFirst(0)
+         ArrayDeque.get(0)      ==> 0
+         ArrayDeque.get(0)      ==> 0
+         ArrayDeque.addFirst(3)
+         ArrayDeque.addFirst(4)
+         ArrayDeque.addLast(5)
+         ArrayDeque.addLast(6)
+         ArrayDeque.addFirst(7)
+         ArrayDeque.get(3)      ==> 0
+         ArrayDeque.removeFirst()     ==> 7
+         ArrayDeque.addFirst(10)
+         ArrayDeque.addFirst(11)
+         ArrayDeque.removeFirst()     ==> 11
+         ArrayDeque.get(2)      ==> 3
+         ArrayDeque.get(0)      ==> 10
+         ArrayDeque.removeLast()      ==> 6
+         ArrayDeque.addLast(16)
+         ArrayDeque.addFirst(17)
+         ArrayDeque.get(1)      ==> 10
+         ArrayDeque.addLast(19)
+         ArrayDeque.addFirst(20)
+         ArrayDeque.removeFirst()     ==> 20
+         ArrayDeque.get(0)      ==> 10
